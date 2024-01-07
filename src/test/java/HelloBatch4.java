ee05a525ba254aaee7bfac33c4af2bb38ec5a866
Eliza Kabirova <elizakabirova@Elizas-Air.lan>
2024-01-07 11:31:25 -0800

    deleted the class

--- src/test/java/HelloBatch4.java
+++ src/test/java/HelloBatch4.java
@@ -1,5 +1,6 @@
 public class HelloBatch4 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("this is my second change");
     }
 }
