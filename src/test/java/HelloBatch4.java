e9aeb2472000b47521cc22b51ae0005918242f4f
Eliza Kabirova <elizakabirova@Elizas-Air.lan>
2024-01-07 12:34:47 -0800

    change the sout

--- src/test/java/HelloBatch4.java
+++ src/test/java/HelloBatch4.java
@@ -1,6 +1,6 @@
 public class HelloBatch4 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello United States!");
         System.out.println("this is my second change");
     }
 // this is the code which was added by another devops
