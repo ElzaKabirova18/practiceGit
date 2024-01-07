5db0f9ff7f31bdc47fa6022069ae038d2ce0b175
Eliza Kabirova <elizakabirova@Elizas-Air.lan>
2024-01-07 10:32:38 -0800

    added HelloWorld functionality

---
+++ src/test/java/HelloBatch4.java
@@ -0,0 +1,5 @@
+public class HelloBatch4 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
--- src/test/java/practice.java
+++ src/test/java/practice.java
@@ -1,5 +1,6 @@
 public class practice {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello i am learning git");
     }
 }
